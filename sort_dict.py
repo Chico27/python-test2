ce2fa4705f9291b7818ef52b5dca264f361bff0a
chico27 <masamasa.987654321@gmail.com>
2019-11-15 09:15:58 +0900

    new

---
+++ sort_dict.py
@@ -0,0 +1,15 @@
+
+num_eng = {'5': 'five', '1': 'one', '7': 'seven', '3':  'three'}
+
+
+print('')
+print(num_eng)
+print(type(num_eng))
+
+
+sorted_num_eng = sorted(num_eng.items())
+
+print('')
+print(sorted_num_eng)
+print(type(sorted_num_eng))
+
