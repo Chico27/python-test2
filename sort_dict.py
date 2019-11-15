8fb9d435164f3fd0b2a2b70ad311074f31b76754
chico27 <masamasa.987654321@gmail.com>
2019-11-15 09:34:23 +0900

    new

--- sort_dict.py
+++ sort_dict.py
@@ -1,11 +1,10 @@
+from collections import OrderedDict
 
 num_eng = {'5': 'five', '1': 'one', '7': 'seven', '3':  'three'}
-
 
 print('')
 print(num_eng)
 print(type(num_eng))
-
 
 sorted_num_eng = sorted(num_eng.items())
 
@@ -13,3 +12,10 @@ print('')
 print(sorted_num_eng)
 print(type(sorted_num_eng))
 
+sorted_num_eng = OrderedDict(sorted_num_eng)
+
+print('')
+print(sorted_num_eng)
+print(type(sorted_num_eng))
+
+print(sorted_num_eng['5'])
