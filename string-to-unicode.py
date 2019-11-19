dc0d2ee3965647d2f5a6d9b190b07f17ade216bf
chico27 <masamasa.987654321@gmail.com>
2019-11-19 20:51:56 +0900

    new

---
+++ string-to-unicode.py
@@ -0,0 +1,19 @@
+
+testString = 'あああ'
+
+print('-testString-')
+print(testString)
+
+print('-type testString-')
+print(type(testString))
+
+print('')
+
+testString = testString.encode('shift-jis')
+
+print('-testString-')
+print(testString)
+
+print('-type testString-')
+print(type(testString))
+
