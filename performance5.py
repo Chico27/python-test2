73902298e939a8f87230f5ba036407463f255d79
chico27 <masamasa.987654321@gmail.com>
2019-11-24 17:13:39 +0900

    new

---
+++ performance5.py
@@ -0,0 +1,19 @@
+import time
+
+test = 'aaa'
+resultList = []
+for j in range(100):
+    start = time.time()
+    for i in range(100000000):
+        if test != 'bbb':
+            pass
+    print(str(j+1) + ' times.')
+    process_time = time.time() - start
+    resultList.append(process_time)
+    print(process_time)
+    print('')
+
+ave = sum(resultList) / len(resultList)
+
+print(resultList)
+print('平均: ' + str(ave))
